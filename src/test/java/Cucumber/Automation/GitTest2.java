e34f39a78caba71a7568026b64ed4fb46d3da271
samrat <chysam@ymail.com>
2020-03-23 22:30:40 -0600

    canadian dev team

--- src/test/java/Cucumber/Automation/GitTest2.java
+++ src/test/java/Cucumber/Automation/GitTest2.java
@@ -4,7 +4,8 @@ public class GitTest2 {
 
 	public static void main(String[] args) {
 		System.out.println("Ayansh Chowdhury");
-
+		System.out.println("Samrat Chowdhury");
+		System.out.println("Debojanee Choudhury");
 	}
 
 }
