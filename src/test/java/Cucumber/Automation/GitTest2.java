e92353cfa3c8cd81df390d50fbde070fd4a4aefc
samrat <chysam@ymail.com>
2020-03-23 20:58:58 -0600

    Canadian gut commit

---
+++ src/test/java/Cucumber/Automation/GitTest2.java
@@ -0,0 +1,10 @@
+package Cucumber.Automation;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("Ayansh Chowdhury");
+
+	}
+
+}
--- src/test/java/Cucumber/Automation/Gittest.java
+++ src/test/java/Cucumber/Automation/Gittest.java
@@ -4,7 +4,10 @@ public class Gittest {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-
+		System.out.println("Adi");
+		System.out.println("Love you baba");
+		System.out.println("Shormi");
+		System.out.println("Good Morning");
 	}
 
 }
