be45947629ae02581d53ecef2c1cf77a758d7461
samrat <chysam@ymail.com>
2020-03-23 21:21:19 -0600

    architect asian dev

--- src/test/java/Cucumber/Automation/Gittest.java
+++ src/test/java/Cucumber/Automation/Gittest.java
@@ -5,9 +5,11 @@ public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Adi");
-		System.out.println("Love you baba");
-		System.out.println("Shormi");
-		System.out.println("Good Morning");
+		System.out.println("Calgary");
+		System.out.println("dhaka");
+		System.out.println("bangladesh");
 	}
+
+	
 
 }
